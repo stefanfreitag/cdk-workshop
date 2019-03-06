01cff7c9f8cc1d9bc9f320167ae7b09b682dfe82
Stefan Freitag <stefan.freitag@udo.edu>
2019-03-06 21:33:12 +0100

    Added APIGateway

--- lib/cdkworkshop-stack.ts
+++ lib/cdkworkshop-stack.ts
@@ -1,14 +1,20 @@
 import cdk = require('@aws-cdk/cdk');
 import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway')
+import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
 
 export class CdkworkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(this, 'HelloHandler', {
+    const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NodeJS810,
       code: lambda.Code.asset('lambda'),
       handler: 'hello.handler'
     });
+    new apigw.LambdaRestApi(this, 'HelloLambda', {
+      handler: hello
+    });
+
   }
 }
