7299636faa4f1bfa9e754fd8d812a2bfead2c9e4
Stefan Freitag <stefan.freitag@udo.edu>
2019-03-06 21:09:19 +0100

    Created lamdba function

--- lib/cdkworkshop-stack.ts
+++ lib/cdkworkshop-stack.ts
@@ -1,8 +1,14 @@
 import cdk = require('@aws-cdk/cdk');
+import lambda = require('@aws-cdk/aws-lambda');
 
 export class CdkworkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NodeJS810,
+      code: lambda.Code.asset('lambda'),
+      handler: 'hello.handler'
+    });
   }
 }
