3a87bbe056f2315ad83091a8ee1578e9c5a522c3
Stefan Freitag <stefan.freitag@udo.edu>
2019-03-06 20:46:28 +0100

    Removed all content from workshop stackw

--- lib/cdkworkshop-stack.ts
+++ lib/cdkworkshop-stack.ts
@@ -1,17 +1,8 @@
-import sns = require('@aws-cdk/aws-sns');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/cdk');
 
 export class CdkworkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkworkshopQueue', {
-      visibilityTimeoutSec: 300
-    });
-
-    const topic = new sns.Topic(this, 'CdkworkshopTopic');
-
-    topic.subscribeQueue(queue);
   }
 }
